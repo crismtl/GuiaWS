358bbd193c0ebab8fb0d2c40b97a471c2ea2d985
Cristian Toaquiza <crissmtl@gmail.com>
2015-11-27 11:40:50 -0500

    Aceptar parámetros de longitud y latitud desde la url

--- routes/places.js
+++ routes/places.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('Hola desde places');
+	res.send('Bienvenidos al WS de Places');
+});
+
+router.get('/get', function(req, res, next) {
+	var longitud = req.query.longitud;
+	var latitud = req.query.latitud;
+	res.send('Su longitud: '+longitud+'y latitud: '+latitud);
 });
 
 module.exports = router;
