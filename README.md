f7e992720229d160e5eed0379a293593ecbf7057
Cristian Toaquiza <crissmtl@gmail.com>
2015-11-28 13:41:22 -0500

    Crear request al servidor de Google Places y mostrarlo en consola

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # GuiaWS 
 ##Dependencias
--Node JS
--NPM
--Nodemon: 'npm install -g nodemon'
--Express Generator: 'npm install -g express-generator'
+- Node JS
+- NPM
+- Nodemon: 'npm install -g nodemon'
+- Express Generator: 'npm install -g express-generator'
 
 ##Configuración
 **Dentro de la carpeta del proyecto**
--Ejecutar 'npm install' para instalar dependecias de node.
--Ejecutar 'nodemon' para levantar el servidor.
+- Ejecutar 'npm install' para instalar dependecias de node.
+- Ejecutar 'nodemon' para levantar el servidor.
--- routes/places.js
+++ routes/places.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
 router.get('/', function(req, res, next) {
 	res.send('Bienvenidos al WS de Places');
@@ -8,7 +9,39 @@ router.get('/', function(req, res, next) {
 router.get('/get', function(req, res, next) {
 	var longitud = req.query.longitud;
 	var latitud = req.query.latitud;
-	res.send('Su longitud: '+longitud+'y latitud: '+latitud);
+
+	//var lugares;
+
+	var opcionesDeServidor = {
+		hostname: 'www.googleapis.com',
+		port: 443,
+		path: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-0.220379,%20-78.5120386&radius=500&types=food&key=AIzaSyBZzx_zsBJQEn5ufg9HqtZl7nDRAbjfApI&',
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	var requestAServidor = https.request(opcionesDeServidor, function(responseDeServidor) {
+		responseDeServidor.setEncoding('utf8');
+		responseDeServidor.on('data', function (chunk) {
+			console.log(chunk);
+			/*aqui se puede usar los datos que se recibe, pero no se puede usar directamente
+			res.send(chunk) porque da un error.
+			la otra opcion era crear una variable global y guardarla con el valor de chunk
+			lugares = chunk, pero no funciona porque manda cuando todavia esta vacia la variable*/
+		});
+		responseDeServidor.on('end', function() {
+			console.log('No more data in response.')
+		})
+	});
+
+	requestAServidor.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	requestAServidor.end();
+	//res.send(lugares);
 });
 
 module.exports = router;
